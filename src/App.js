7dc6a75ee24f234656fc65c2b6442fbabd86fa5a
Ju111 <SchneiderJu@gmx.net>
2018-02-23 21:34:42 +0100

    New Project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import LogoImg from './game.png';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img id="logo" src={LogoImg} alt=''/>
+          <h1 className="App-title">HANGMAAAAAN</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
