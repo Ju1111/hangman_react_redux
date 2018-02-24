2863bc01c496c8d6e2ee89c50e683e914cdf4570
Ju111 <SchneiderJu@gmx.net>
2018-02-24 20:53:46 +0100

    word guess reducer refactored

--- src/reducers/word.js
+++ src/reducers/word.js
@@ -3,16 +3,18 @@ import { LETTER_GUESSED } from '../actions/types'
 const initialState = {
   wrongGuessCount: 0,
   guesses: [],
-  word: '',
+  word: 'coding',
 }
 
 export default (state = initialState, { type, payload } = {}) => {
   switch (type) {
     case LETTER_GUESSED :
-    state.guesses.push(payload);
-    state.guesses += 1;
-    return state;
-  default:
-    return state
+      if (state.word.indexOf(payload) === -1) {
+        state.guesses += 1};
+      return {
+        ...state, guesses:
+      state.guesses.contact(payload)};
+      default:
+        return state
   }
 }
