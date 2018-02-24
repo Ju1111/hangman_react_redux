8e04061bc49a18ec894a1d4d6680c47d8da78354
Ju111 <SchneiderJu@gmx.net>
2018-02-24 20:27:36 +0100

    reducer updates wrongGuessCount and guesses

--- src/reducers/word.js
+++ src/reducers/word.js
@@ -1,9 +1,18 @@
+import { LETTER_GUESSED } from '../actions/types'
+
 const initialState = {
   wrongGuessCount: 0,
   guesses: [],
   word: '',
 }
 
-export default function(state = initialState, action = {}) {
-  return state
+export default (state = initialState, { type, payload } = {}) => {
+  switch (type) {
+    case LETTER_GUESSED :
+    state.guesses.push(payload);
+    state.guesses += 1;
+    return state;
+  default:
+    return state
+  }
 }
